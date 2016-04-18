28ee8b8acdc153889de1f4e0462490764978a0db
Iris Caffin <iriscaffin@gmail.com>
2016-04-18 11:12:52 -0700

    Fibonacci Sequence Recursive Approach in C#

---
+++ FibonacciSequence2/FibonacciSequence2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSequence2
+{
+    class Program
+    {
+        public static int FibonacciSequence2(int n)
+        {
+            if (n == 0) return 0; // To return the first Fibonacci number
+            if (n == 1) return 1; // To return the second Fibonacci number
+            return FibonacciSequence2(n - 1) + FibonacciSequence2(n - 2);
+        }
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the length of the Fibonacci Sequence: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("{0} ", FibonacciSequence2(i));
+            }
+            Console.ReadKey();
+        }
+    }
+}
